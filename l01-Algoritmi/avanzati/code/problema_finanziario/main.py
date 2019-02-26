2feb78384782fe23317e2634ed8d8650a49d4008
dido18 <davideneri18@gmail.com>
2019-02-26 15:56:19 +0100

    avanzati: spiegazione problema finananziario, multikey quicksort load function

--- l01-Algoritmi/avanzati/code/problema_finanziario/main.py
+++ l01-Algoritmi/avanzati/code/problema_finanziario/main.py
@@ -1,26 +1,25 @@
 import time
-
+import random
 from lineare import lineare
 from quadratico import quadratico
 from cubico import cubico
 
-d = [10, +4, -6, +3, +1, +3, -2, +3, -4, +1, -9, +6]
+num_days = 10000 # number of days (length of d array)
+d = [random.randrange(-10,10) for num in range(num_days)]
 
-
-start = time.time() # segna il tempo di inizio nella variabile start
+start = time.time() 
 cubico(d)
-stop = time.time() # segna il tempo di fine nella variabile start
+stop = time.time() 
 print('Cubico ', stop-start, "secondi.")
 
-start = time.time() # segna il tempo di inizio nella variabile start
+start = time.time() 
 quadratico(d)
-stop = time.time() # segna il tempo di fine nella variabile start
+stop = time.time() 
 print('Quadratico ', stop-start, "secondi.")
 
-
-start = time.time() # segna il tempo di inizio nella variabile start
+start = time.time() 
 lineare(d)
-stop = time.time() # segna il tempo di fine nella variabile start
+stop = time.time() 
 print('Lineare ', stop-start, "secondi.")
 
 
