15071238b23325ff03bf293bb53d723e46a9c9e3
dido18 <davideneri18@gmail.com>
2019-03-05 18:29:42 +0100

    slides OTP

--- l02-Crittografia/code/OneTimePad/otp.py
+++ l02-Crittografia/code/OneTimePad/otp.py
@@ -1,4 +1,4 @@
-from utils import from_char_to_value, from_value_to_char
+from utils import from_char_to_value, from_value_to_char, create_otp_key
 
 def otp_encrypt(msg, key):
      result = ''
@@ -21,9 +21,21 @@ def otp_decrypt(msg, key):
 
 if __name__ == '__main__':
     
-    msg = "ciao" #input("Message: ")
-    key = "ZUBQ" # input("Key: ")
+    msg = "CIAO" 
+    key = "AJRF" 
 
-    cifrato = otp_encrypt(msg, key)
+#     cifrato = otp_encrypt(msg, key)
+#     print("Messaggio cifrato: ", cifrato)
+#     inchiaro =  otp_decrypt(cifrato,key)
+#     print("Messaggio decifrato: ", inchiaro )
+
+    promessi = "Quel ramo del lago di Como, che volge a mezzogiorno, tra due catene non interrotte di monti"
+    promessi = promessi.replace(" ", "")
+    promessi = promessi.replace(",", "")
+
+    key = create_otp_key(len(promessi))
+    print("Key: ", key)
+    cifrato = otp_encrypt(promessi, key)
     print("Messaggio cifrato: ", cifrato)
-    print("Messaggio decifrato: ", otp_decrypt(cifrato,key))
+    inchiaro =  otp_decrypt(cifrato, key)
+    print("Messaggio decifrato: ", inchiaro)
